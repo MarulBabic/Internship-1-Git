cf94b255d75397907680e52ea220ce5315a8cac0
MarulBabic <167626236+MarulBabic@users.noreply.github.com>
2024-11-06 15:38:33 +0100

    zavrsen zadatak: permutacija

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         TicTacToe game = new TicTacToe();
         game.playTicTacToe();
+
+        Permutation permutation = new Permutation();
+        permutation.generatePermutation();
     }
 }
